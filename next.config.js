d7787d93ddaea8be6500b65602fde390019b400f
pinglu85 <luping8576@hotmail.com>
2022-09-06 17:25:44 +0200

    enable importing svg as component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'removeTitle',
+                  active: false,
+                },
+                {
+                  // Make SVG responsive
+                  name: 'removeViewBox',
+                  active: false,
+                },
+                {
+                  name: 'removeDimensions',
+                  active: true,
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;

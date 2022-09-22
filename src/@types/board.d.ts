aa49e4f7a0c13a72c554965d1bfda97adc22eceb
pinglu85 <luping8576@hotmail.com>
2022-09-22 16:06:27 +0200

    refactor(@types): move type definitions related to `board` into a separate file

---
+++ src/@types/board.d.ts
@@ -0,0 +1,18 @@
+interface List {
+  id: string;
+  name: string;
+  closed: boolean;
+  boardId: string;
+}
+
+interface Card {
+  id: string;
+  boardId: string;
+  listId: string;
+  name: string;
+}
+
+interface BoardData {
+  lists: List[];
+  cards: Card[];
+}

3fb4140dbe43516bda92d3fa9cdd83d4a5468a42
pinglu85 <luping8576@hotmail.com>
2022-09-25 20:56:06 +0200

    feat(common): implement `Menu` component

---
+++ src/components/common/Menu/Menu/index.tsx
@@ -0,0 +1,26 @@
+import { forwardRef } from 'react';
+
+import joinClassNames from '../../../../utils/joinClassNames';
+import styles from './styles.module.css';
+
+interface MenuProps extends WithChildrenProps {
+  className?: string;
+}
+
+type Ref = HTMLDivElement;
+
+const Menu = forwardRef<Ref, MenuProps>(
+  ({ className = '', children }, ref): JSX.Element => {
+    return (
+      <div ref={ref} className={joinClassNames(styles.Menu, className)}>
+        {children}
+      </div>
+    );
+  }
+);
+// If we don't set the `displayName` property on the `Menu` component,
+// we will get an error of `Component definition is missing display name`
+// from eslint.
+Menu.displayName = 'Menu';
+
+export default Menu;
---
+++ src/components/common/Menu/MenuDivider/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+
+const MenuDivider = (): JSX.Element => {
+  return <hr className={styles.MenuDivider} />;
+};
+
+export default MenuDivider;
